2749f27e584b7ce949acc5d5272a043be9aa718a
burnout131 <marcrg07@gmail.com>
2023-04-20 10:47:07 +0200

    Initial commit Ejercicio 1

---
+++ TA25_Ejercicio1/src/main/java/com/mrojo/ta25/Ta25Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25Ejercicio1Application.class, args);
+	}
+
+}
