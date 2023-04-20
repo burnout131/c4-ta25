2e5957af175ef66a657d978f162c268bcbaed7f9
burnout131 <marcrg07@gmail.com>
2023-04-20 18:31:42 +0200

    Ejercicio 3 initial upload

---
+++ TA25_Ejercicio3/src/main/java/com/mrojo/ta25ej3/Ta25Ejercicio3Application.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta25ej3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta25Ejercicio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta25Ejercicio3Application.class, args);
+	}
+
+}
---
+++ TA25_Ejercicio3/src/test/java/com/mrojo/ta25ej3/Ta25Ejercicio3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrojo.ta25ej3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ta25Ejercicio3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
